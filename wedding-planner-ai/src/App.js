fa93e52a23b680e57ab2bbd8806864c60027a78e
Kat <91990952+Rebelkat18@users.noreply.github.com>
2024-02-27 13:02:11 -0500

    server

--- wedding-planner-ai/src/App.js
+++ wedding-planner-ai/src/App.js
@@ -1,9 +1,10 @@
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import Message from "./components/Message";
 import Input from "./components/Input";
 import History from "./components/History";
 import Clear from "./components/Clear";
+import OpenAI from "openai";
 
 import "./App.css";
 
@@ -12,39 +13,56 @@ export default function App() {
   const [messages, setMessages] = useState([]);
   const [history, setHistory] = useState([]);
 
-  const handleSubmit = async () => {
-    const prompt = {
-      role: "user",
-      content: input,
-    };
+  // const openai = new OpenAI();
 
-    setMessages([...messages, prompt]);
+  useEffect(() => {
+    fetch("http://localhost:8000/message")
+      .then((res) => res.json())
+      .then((data) => setMessages(data.message));
+  }, []);
 
-    await fetch("https://api.openai.com/v1/chat/completions", {
-      method: "POST",
-      headers: {
-        Authorization: `Bearer ${process.env.REACT_APP_OPENAI_API_KEY}`,
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        model: "gpt-3.5-turbo",
-        messages: [...messages, prompt],
-      }),
-    })
-      .then((data) => data.json())
-      .then((data) => {
-        const res = data.choices[0].message.content;
-        setMessages((messages) => [
-          ...messages,
-          {
-            role: "assistant",
-            content: res,
-          },
-        ]);
-        setHistory((history) => [...history, { question: input, answer: res }]);
-        setInput("");
-      });
-  };
+
+  // const handleSubmit = async () => {
+  //   const prompt = {
+  //     role: "user",
+  //     content: input,
+  //   };
+
+  //   setMessages([...messages, prompt]);
+
+  //   const completion = await openai.chat.completions.create({
+  //     messages: [{ role: "system", content: "You are a helpful assistant." }],
+  //     model: "gpt-3.5-turbo",
+  //   });
+  
+  //   console.log(completion.choices[0]);
+
+  //   await fetch("https://api.openai.com/v1/chat/completions", {
+  //     method: "POST",
+  //     headers: {
+  //       Authorization: `Bearer ${process.env.REACT_APP_OPENAI_API_KEY}`,
+  //       "Content-Type": "application/json",
+  //     },
+  //     body: JSON.stringify({
+  //       model: "gpt-3.5-turbo",
+  //       messages: [...messages, prompt],
+  //     }),
+  //   })
+  //     .then((data) => data.json())
+  //     .then((data) => {
+  //       console.log(data);
+  //       const res = data.choices[0].message.content;
+  //       setMessages((messages) => [
+  //         ...messages,
+  //         {
+  //           role: "assistant",
+  //           content: res,
+  //         },
+  //       ]);
+  //       setHistory((history) => [...history, { question: input, answer: res }]);
+  //       setInput("");
+  //     });
+  // };
 
   const clear = () => {
     setMessages([]);
@@ -55,15 +73,17 @@ export default function App() {
     <div className="App">
       <div className="Column">
         <h3 className="Title">Chat Messages</h3>
+        <h1>{messages}</h1>
+
         <div className="Content">
-          {messages.map((el, i) => {
+          {/* {messages.map((el, i) => {
             return <Message key={i} role={el.role} content={el.content} />;
-          })}
+          })} */}
         </div>
         <Input
           value={input}
           onChange={(e) => setInput(e.target.value)}
-          onClick={input ? handleSubmit : undefined}
+          // onClick={input ? handleSubmit : undefined}
         />
       </div>
       <div className="Column">
